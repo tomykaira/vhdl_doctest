023e1e2949c0090347589e1556c58ffa53af7cdd
tomykaira <tomykaira@gmail.com>
2012-08-23 13:47:26 +0900

    Pass DUT end-to-end test

--- lib/vhdl_doctest/dut.rb
+++ lib/vhdl_doctest/dut.rb
@@ -6,7 +6,7 @@ module VhdlDoctest
     end
 
     attr_accessor :entity, :ports, :cases
-    def new(entity, ports, cases)
+    def initialize(entity, ports, cases)
       @entity, @ports, @cases = entity, ports, cases
     end
 
@@ -20,21 +20,48 @@ module VhdlDoctest
         ports = extract_ports
         cases = extract_test_cases
 
-        [entity, ports, Hash[cases.map { |c| ports.zip(c) }]]
+        [entity, ports, cases.map{ |c| TestCase.new(Hash[ports.zip(c)]) }]
       end
 
       def extract_ports
-        ports = []
+        return @ports if @ports
+        @ports = []
         definitions = @vhdl.match(/entity.*is\s+port\s*\((.*)\);\s*end/m)[1]
         definitions.split("\n").each do |l|
           names, attributes = l.strip.gsub(/;$/, '').split(":")
           next unless attributes
           destination, type = attributes.strip.split(' ', 2)
           names.split(',').each do |name|
-            ports << Port.new(name.strip, destination.to_sym, VhdlDoctest::Types.parse(type))
+            @ports << Port.new(name.strip, destination.to_sym, VhdlDoctest::Types.parse(type))
           end
         end
-        ports
+        @ports
+      end
+
+      def extract_test_cases
+        definitions = @vhdl.match(/-- TEST\n(.*)-- \/TEST/m)[1]
+        header, *body = definitions.split("\n").map { |l| l[3..-1].split("|").map(&:strip) }
+
+        header.each_with_index do |h, idx|
+          radix = 10
+          if h.include?(' ')
+            case h[-1]
+            when 'b'
+              radix = 2
+            when 'h', 'x'
+              radix = 16
+            end
+          end
+          prev = ''
+          body.each do |l|
+            if l[idx].empty?
+              l[idx] = prev
+            else
+              prev = l[idx] = l[idx].to_i(radix)
+            end
+          end
+        end
+        body
       end
     end
   end
