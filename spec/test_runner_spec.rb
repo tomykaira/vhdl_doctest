6e0728eb4e5f048bcf44d65da71bc0088766bf5f
tomykaira <tomykaira@gmail.com>
2012-09-10 12:39:46 +0900

    Fix that minus decimal value is converted wrong binary in testbench

--- spec/test_runner_spec.rb
+++ spec/test_runner_spec.rb
@@ -4,32 +4,33 @@ module VhdlDoctest
   describe TestRunner do
     let(:dut_file) { "examples/alu.vhd/" }
     describe "#run" do
+      let(:out) { StringIO.new }
+      subject { out.rewind; out.read }
+      before { described_class.new(out, dut_file, file).run }
+
       context "3 successful examples" do
-        let(:out) { StringIO.new }
         let(:file) { test_file([[18, 9, 0, 0, 1], [18, 18, 7, 0, 1], [18, 19, 7, 1, 0]]) }
-        subject { out.rewind; out.read }
-        before { described_class.new(out, dut_file, file).run }
 
         it { should match "3 examples, 0 failures" }
       end
 
       context "1 successful examples" do
-        let(:out) { StringIO.new }
         let(:file) { test_file([[18, 9, 0, 0, 1]]) }
-        subject { out.rewind; out.read }
-        before { described_class.new(out, dut_file, file).run }
 
         it { should match "1 examples, 0 failures" }
       end
 
       context "1 failing examples" do
-        let(:out) { StringIO.new }
         let(:file) { test_file([[18, 9, 0, 0, 0]]) }
-        subject { out.rewind; out.read }
-        before { described_class.new(out, dut_file, file).run }
 
         it { should match "1 examples, 1 failures" }
         it { should match "actual:" }
+      end
+
+      context "stimuli including minus" do
+        let(:file) { test_file([[18, -9, 2, 9, 0]]) }
+
+        it { should match "1 examples, 0 failures" }
       end
     end
 
