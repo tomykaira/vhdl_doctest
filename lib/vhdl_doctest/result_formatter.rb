11788d80d9d5e601ead2a2e166e0a6a78baf927d
tomykaira <tomykaira@gmail.com>
2012-09-10 11:36:30 +0900

    Pretty print failed case

--- lib/vhdl_doctest/result_formatter.rb
+++ lib/vhdl_doctest/result_formatter.rb
@@ -4,16 +4,38 @@ module VhdlDoctest
       @output = output
     end
 
+    # pretty print error message
     def format
       if compile_error?
         "FAILED: Test did not run because of compilation error"
       else
-        @output
+        format = []
+        @output.split("\n").each do |l|
+          if l.match(/(FAILED: .*) expected to (.*), but (.*)/)
+            format << $1
+            format << "  expected: " + $2
+            format << "    actual: " + replace_binary($3)
+          end
+        end
+        format.join("\n")
       end
     end
 
     def compile_error?
       @output.include?("ghdl: compilation error")
+    end
+
+    def succeeded?
+      ! compile_error? && ! failed?
+    end
+
+    def failed?
+      @output.include?("FAILED")
+    end
+
+    # convert binary expression in a string to decimal
+    def replace_binary(str)
+      str.gsub(/[01]+/) { |bin| bin.to_i(2).to_s(10) }
     end
   end
 end
--- spec/result_formatter_spec.rb
+++ spec/result_formatter_spec.rb
@@ -18,8 +18,38 @@ ghdl: Please elaborate your design.
 }}
 
       it { should be_compile_error }
+      it { should_not be_succeeded }
       its(:format) { should match /FAIL/ }
       its(:format) { should match /Test did not run/ }
+    end
+
+    context "test run, but one case failed" do
+      let(:output) { %q{/tmp/d20120910-14672-k0mvgz/testbench_main_decoder.vhd:587:1:@790ns:(assertion warning): FAILED: op = 44, rx_done = 0, stage = 1 expected to next_stage = 2, bus_to_reg = 0, alu_src = 0, pc_src = 0, reg_dst = 1, alu_control = 0, mem_write = 0, send_enable = 0, reg_write = 0, rx_enable = 0, but next_stage = 0010, bus_to_reg = 0, alu_src = 1, pc_src = 000, reg_dst = 0, alu_control = 0000, mem_write = 0, send_enable = 0, reg_write = 0, rx_enable = 0} }
+
+      it { should_not be_compile_error  }
+      it { should_not be_succeeded }
+      it "should pretty print the result" do
+        subject.format.should include %q{
+FAILED: op = 44, rx_done = 0, stage = 1
+  expected: next_stage = 2, bus_to_reg = 0, alu_src = 0, pc_src = 0, reg_dst = 1, alu_control = 0, mem_write = 0, send_enable = 0, reg_write = 0, rx_enable = 0
+    actual: next_stage = 2, bus_to_reg = 0, alu_src = 1, pc_src = 0, reg_dst = 0, alu_control = 0, mem_write = 0, send_enable = 0, reg_write = 0, rx_enable = 0
+}.strip
+      end
+    end
+
+    describe '#replace_binary' do
+      def replace_binary(str)
+        ResultFormatter.new('').replace_binary(str)
+      end
+
+      specify do
+        expect(replace_binary('next_stage = 0010')).to eq('next_stage = 2')
+      end
+
+      specify do
+        expect(replace_binary('next_stage = 0010, alu_control = 0000')).
+          to eq('next_stage = 2, alu_control = 0')
+      end
     end
   end
 end
